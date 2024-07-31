571261540d5fbae24d882eb2c58144df6ffef876
Shohruz Ernazarov <shohruz123456789@gmail.com>
2024-07-31 16:19:30 -0400

    Add firebase and firestore implementation

---
+++ lib/firebase.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: "inventory-manager-d3325.firebaseapp.com",
+  projectId: "inventory-manager-d3325",
+  storageBucket: "inventory-manager-d3325.appspot.com",
+  messagingSenderId: "797575731286",
+  appId: "1:797575731286:web:f70a12b2c5507b0a8f43f7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { app, db };
