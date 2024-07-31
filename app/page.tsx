f30e3aa498f709f14860ff2f98654fd11e50564f
Shohruz Ernazarov <shohruz123456789@gmail.com>
2024-07-31 16:20:25 -0400

    Add ItemList component and structure into 2 columns

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,31 @@
-import { Box, Container, Typography } from "@mui/material";
+import { Box, Container, Typography, Grid } from "@mui/material";
+import ItemList from "../components/ItemList";
+import AddItem from "@/components/AddItem";
 
 const items = ['item1', 'item2', 'item3', 'item4', 'item5', 'item6', 'item7', 'item8', 'item9', 'item10'];
 
 export default function Home() {
+
   return (
-    <Container>
+    <Container sx={{ width:'100vw', height:"100vh", overflow:'hidden' }}>
       <Box
-        width="100vw"
-        height="100vh"
         display={'flex'}
         flexDirection={'column'}
         justifyContent={'center'}
         alignItems={'center'}
-        gap={2}
+        gap={3}
       >
-        <Box sx={{ textAlign: 'center' }}>
+        <Box sx={{ textAlign:'center', padding: '0.25rem', borderBottom:'2px solid black', marginTop:"4rem", marginBottom:"2rem" }}>
           <Typography variant="h4">Inventory Manager</Typography>
         </Box>
-        <Box sx={{ width: '500px' }}>
-          {items.map((item, index) => (
-            <Box 
-              key={index}
-              sx={{ border: '2px solid black', padding: '0.5rem 1rem', marginBottom: '1rem' }}
-            >
-              <Typography variant="h6">{item}</Typography>
-            </Box>
-          ))}
-        </Box>
+        <Grid container columns={12} justifyContent="center" sx={{ gap:"2rem" }}>
+          <Grid xs>
+            <AddItem />
+          </Grid>
+          <Grid xs>
+            <ItemList />
+          </Grid>
+        </Grid>
       </Box>
     </Container>
   );
