56feb6aeb61a93f664ca0376840e4a046409d702
Shohruz Ernazarov <shohruz123456789@gmail.com>
2024-07-31 22:16:48 -0400

    Test: Comment out code for quantity filter slider

--- components/ItemList.jsx
+++ components/ItemList.jsx
@@ -1,13 +1,14 @@
 'use client';
 
 import React, { useState, useEffect } from 'react'
-import { Stack, Box, Typography, Button } from '@mui/material'
+import { Stack, Box, Typography, Button, TextField, Slider } from '@mui/material'
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 
 import { collection, getDocs } from "firebase/firestore";
 
 import { db } from '../lib/firebase';
+import { getItemsByQuantityFilter } from '../lib/util';
 import EditModal from './EditModal';
 import DeleteModal from './DeleteModal';
 
@@ -17,6 +18,9 @@ export default function ItemList() {
   const [isEditModalOpen, setIsEditModalOpen] = useState(false); 
   const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
   const [selectedItem, setSelectedItem] = useState(null);
+  const [searchQuery, setSearchQuery] = useState('');
+  // const [quantityFilterValue, setQuantityFilterValue] = useState([0, 100]);
+  // const [isFilterApplied, setIsFilterApplied] = useState(false);
 
   useEffect(() => {
     async function getItems() {
@@ -29,16 +33,29 @@ export default function ItemList() {
         setItems(items);
     }
     getItems();
-  }, [selectedItem]);
+  }, []);
+
+  // useEffect(() => {
+  //   async function getItems() {
+  //     if (isFilterApplied) {
+  //       const items = await getItemsByQuantityFilter(quantityFilterValue[0], quantityFilterValue[1]);
+  //       setItems(items);
+  //       setIsFilterApplied(false);
+  //     }
+  //   }
+  //   if (isFilterApplied) {
+  //     getItems();
+  //   }
+  // }, [isFilterApplied]);
 
   const handleUpdate = (item) => {
-    console.log('Update');
+    // console.log('Update');
     setSelectedItem(item);
     setIsEditModalOpen(true);
   }
 
   const handleDelete = (item) => {
-    console.log('Delete');
+    // console.log('Delete');
     setSelectedItem(item);
     setIsDeleteModalOpen(true);
   }
@@ -48,11 +65,50 @@ export default function ItemList() {
     setIsDeleteModalOpen(false);
   };
 
+  // const getQuantityFilter = (value) => { 
+  //   return `${value}`;
+  // }
+
+  // const handleQuantityFilterChange = (event, newValue) => {
+  //   setQuantityFilterValue(newValue);
+  // };
+
   return (
     <Box>
       <Typography variant='h4' textAlign={'center'} sx={{ marginBottom:'2rem'}}>Items</Typography>
+        <Box width="100%" marginBottom={4}>
+          <TextField
+            color="success"
+            fullWidth={true}
+            id="outlined-basic" 
+            label="Search..." 
+            variant="filled"   
+            value={searchQuery}
+            onChange={(event) => {
+              setSearchQuery(event.target.value);
+            }}
+          />
+        </Box>
+        {/* <Box width="100%">
+          <Slider
+            aria-label='Quantity'
+            getAriaValueText={getQuantityFilter}
+            valueLabelDisplay='auto'
+            onChange={handleQuantityFilterChange}
+            value={quantityFilterValue}
+            step={1}
+          />
+        </Box>
+      
+      <Box textAlign={'right'} sx={{ marginTop:'1rem', marginBottom:'1rem'}}>
+        <Button variant="outlined" onClick={setIsFilterApplied(true)}>
+          Apply
+        </Button>
+      </Box> */}
       <Stack spacing={1} overflow={'auto'} sx={{ width:'100%', height:'50vh' }}>
-        {items.map((item, index) => (
+        {items.filter((item) => { 
+          return searchQuery.toLowerCase() === "" ? item : item.name.toLowerCase().includes(searchQuery);
+        }).map((item, index) => (
           <Box 
             key={index}
             sx={{
