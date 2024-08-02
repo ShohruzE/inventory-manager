2672f547b1b2c0a97d61f4096a22b924da4738c0
Shohruz Ernazarov <shohruz123456789@gmail.com>
2024-08-02 12:42:49 -0400

    Remove unnecessary title

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import AddItem from "@/components/AddItem";
 export default function Home() {
 
   return (
-    <Container sx={{ width:'100vw', height:"100vh", overflow:'hidden' }}>
+    <Container maxWidth="xl" sx={{ width:'100vw', height:"100vh", overflow:'hidden' }}>
       <Box
         display={'flex'}
         flexDirection={'column'}
@@ -13,14 +13,14 @@ export default function Home() {
         alignItems={'center'}
         gap={3}
       >
-        <Box sx={{ textAlign:'center', padding: '0.25rem', borderBottom:'2px solid black', marginTop:"4rem", marginBottom:"2rem" }}>
+        {/* <Box sx={{ textAlign:'center', padding: '0.25rem', borderBottom:'2px solid black', marginTop:"4rem", marginBottom:"2rem" }}>
           <Typography variant="h4">Inventory Manager</Typography>
-        </Box>
-        <Grid container columns={12} justifyContent="center" sx={{ gap:"2rem" }}>
-          <Grid xs>
+        </Box> */}
+        <Grid container columns={12} justifyContent="center" sx={{ gap:"2rem", marginTop:"2rem"}}>
+          <Grid md>
             <AddItem />
           </Grid>
-          <Grid xs>
+          <Grid md>
             <ItemList />
           </Grid>
         </Grid>
