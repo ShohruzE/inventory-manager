98ec6212625255b0ff0823937a82a0532d6b10de
Shohruz Ernazarov <shohruz123456789@gmail.com>
2024-08-04 11:01:02 -0400

    Add user auth to firestore calls

--- lib/util.ts
+++ lib/util.ts
@@ -1,7 +1,8 @@
-import { db } from "./firebase";
-import { collection, getDocs, DocumentData, setDoc, doc, deleteDoc, updateDoc, serverTimestamp, addDoc, query, where } from "firebase/firestore"; 
+import { db, auth, storage } from "./firebase";
+import { collection, getDocs, DocumentData, getDoc, setDoc, doc, deleteDoc, updateDoc, serverTimestamp, addDoc, query, where } from "firebase/firestore"; 
 
 type Item = {
+    userId: string;
     id: string;
     name: string;
     quantity: number;
@@ -9,48 +10,99 @@ type Item = {
 }
 
 export async function getItems() {
-    const querySnapshot = await getDocs(collection(db, "inventory"));
-    const items: DocumentData[] = [];
-    querySnapshot.forEach((doc) => {
-      items.push(doc.data());
-    });
-    return items;
+
+    try {
+        const userId = auth?.currentUser?.uid;
+        if (!userId) throw new Error('User is not authenticated');
+    
+    
+        const q = query(collection(db, "inventory"), where('id', '==', userId));
+        const querySnapshot = await getDocs(q);
+        const items: DocumentData[] = [];
+        querySnapshot.forEach((doc) => {
+          items.push(doc.data());
+        });
+        return items;
+    }
+    catch (error) {
+        console.error('Error getting documents: ', error);
+    }
 }
 
 export async function addItem(name: string, quantity: number) {
-    const docRef = await addDoc(collection(db, "inventory"), {
-        name: name,
-        quantity: quantity,
-        timestamp: serverTimestamp()
-    });
-    await setDoc(doc(db, "inventory", docRef.id), {
-        id: docRef.id
-    }, { merge: true }
-    );
+
+    try {
+        const userId = auth?.currentUser?.uid;
+        if (!userId) throw new Error("User not authenticated");
+    
+        const docRef = await addDoc(collection(db, "inventory"), {
+            userId: userId,
+            name: name,
+            quantity: quantity,
+            timestamp: serverTimestamp()
+        });
+        await setDoc(doc(db, "inventory", docRef.id), {
+            id: docRef.id
+        }, { merge: true });
+    }
+    catch (error) {
+        console.error('Error adding document: ', error);
+    }
+
 }
 
 export async function deleteItem(id: string) {
-    await deleteDoc(doc(db, "inventory", id));
+
+    try {
+        const userId = auth?.currentUser?.uid;
+        if (!userId) throw new Error("User not authenticated");
+    
+        const itemRef = doc(db, "inventory", id);
+        const itemDoc = await getDoc(itemRef);
+        if (itemDoc.exists() && itemDoc.data().userId === userId) {
+            await deleteDoc(itemRef);
+        } else {
+            throw new Error("Unauthorized or item not found");
+        }
+    }
+    catch (error) {
+        console.error('Error deleting document: ', error);
+    }
 }
 
 export async function updateItem(id: string, name: string, quantity: number) {
-    const itemRef = doc(db, "inventory", id);
-    await updateDoc(itemRef, {
-        name: name,
-        quantity: quantity,
-        timestamp: serverTimestamp()
-    });
+
+    try {
+        const userId = auth?.currentUser?.uid;
+        if (!userId) throw new Error("User not authenticated");
+    
+        const itemRef = doc(db, "inventory", id);
+        const itemDoc = await getDoc(itemRef);
+        if (itemDoc.exists() && itemDoc.data().userId === userId) {
+            await updateDoc(itemRef, {
+                name: name,
+                quantity: quantity,
+                timestamp: serverTimestamp()
+            });
+        } else {
+            throw new Error("Unauthorized or item not found");
+        }
+    }
+    catch (error) {
+        console.error('Error updating document: ', error);
+    }
+
 }
 
-export async function getItemsByQuantityFilter(min: number, max: number) {
-    const q = query(collection(db, "inventory"),
-        where('quantity', '>=', min),
-        where('quantity', '<=', max));
-    const querySnapshot = await getDocs(q);
-    const items: DocumentData[] = [];
-    querySnapshot.forEach((doc) => {
-      items.push(doc.data());
-    });
-    console.log(items);
-    return items;
-}
+// export async function getItemsByQuantityFilter(min: number, max: number) {
+//     const q = query(collection(db, "inventory"),
+//         where('quantity', '>=', min),
+//         where('quantity', '<=', max));
+//     const querySnapshot = await getDocs(q);
+//     const items: DocumentData[] = [];
+//     querySnapshot.forEach((doc) => {
+//       items.push(doc.data());
+//     });
+//     console.log(items);
+//     return items;
+// }
